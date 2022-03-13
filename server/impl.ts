e5355443167f065ba9eff26f54d42f86f8446149
mkrupa <marcin.krupa@student.wat.edu.pl>
2022-03-14 00:10:18 +0100

    almost all

--- server/impl.ts
+++ server/impl.ts
@@ -39,7 +39,7 @@ export class Impl implements Methods<InternalState> {
       players: [],
       currentTurn: Color.GRAY,
       cards: [],
-      roundInfo: {cards: 5, hints:0, timeMax: 60, timeLeft: 60, board: undefined},
+      roundInfo: { cards: 5, hints: 0, timeMax: 60, timeLeft: 60, board: undefined },
       gameStatus: GameStatus.NOT_STARTED,
       teams: teams,
     };
@@ -50,24 +50,39 @@ export class Impl implements Methods<InternalState> {
       return Response.error("Already joined");
     }
     state.players.push(createPlayer(userId));
+
     return Response.ok();
   }
 
   startGame(state: InternalState, userId: UserId, ctx: Context, request: IStartGameRequest): Response {
+    let player = state.players.find((p) => p.id === userId);
+    if (!player) {
+      return Response.error("Player is undefined");
+    }
+
     if (state.gameStatus == GameStatus.AUCTION || state.gameStatus == GameStatus.GUESSING) {
       return Response.error("Game already started");
     }
-//  if(state.players.length < 4) return Response.error("Not enough players");
 
-    state.players = ctx.chance.shuffle(state.players);
-    for (let i = 0; i < state.players.length; i++) {
+    state.roundInfo = {
+      ...state.roundInfo!,
+      cards: 5,
+      hints: 0,
+      timeMax: 90,
+      timeLeft: 90,
+      board: undefined
+    };
+    //  if(state.players.length < 4) return Response.error("Not enough players");
 
-      state.players[i].team = i * 2 < state.players.length ? Color.RED : Color.BLUE;
-      state.players[i].isGivingClues = false;
-    }
+    // state.players = ctx.chance.shuffle(state.players);
+    // for (let i = 0; i < state.players.length; i++) {
 
-    state.players[0].isGivingClues = true;
-    state.players[state.players.length - 1].isGivingClues = true;
+    //   state.players[i].team = i * 2 < state.players.length ? Color.RED : Color.BLUE;
+    //   state.players[i].isGivingClues = false;
+    // }
+
+    // state.players[0].isGivingClues = true;
+    // state.players[state.players.length - 1].isGivingClues = true;
 
 
     const shuffledList = ctx.chance.shuffle(wordList);
@@ -82,7 +97,7 @@ export class Impl implements Methods<InternalState> {
     //losu team 
     state.currentTurn = Color.RED;
 
-    state.teams.forEach(function(team) {
+    state.teams.forEach(function (team) {
       team.bid = 25;
     });
 
@@ -90,28 +105,37 @@ export class Impl implements Methods<InternalState> {
   }
 
   joinTeam(state: InternalState, userId: UserId, ctx: Context, request: IJoinTeamRequest): Response {
-    if (state.gameStatus == GameStatus.GUESSING) {
+    let player = state.players.find((p) => p.id === userId);
+    if (!player) {
+      return Response.error("Player is undefined");
+    }
+
+    if (request.team == Color.GRAY) {
+      player!.team = request.team;
+      player?.isGivingClues == false;
+      return Response.ok();
+    }
+
+    if (player?.isGivingClues && state.gameStatus == GameStatus.GUESSING) {
       return Response.error("You can't join while team is guessing");
     }
 
-    let player = state.players.find((p) => p.id === userId);
-
     player!.team = request.team;
-    if (request.team == Color.GRAY) {
-      player?.isGivingClues == false;
-    }
 
     return Response.ok();
   }
 
   joinAsLeader(state: InternalState, userId: UserId, ctx: Context, request: IJoinAsLeaderRequest): Response {
+    let player = state.players.find((p) => p.id === userId);
+    if (!player) {
+      return Response.error("Player is undefined");
+    }
+
     if (state.gameStatus == GameStatus.GUESSING) {
       return Response.error("Game already started");
     }
 
-    let player = state.players.find((p) => p.id === userId);
-
-    if (player!.team = Color.GRAY) {
+    if (player!.team !== Color.GRAY) {
       let playerGivingClues = state.players.find((p) => {
         if (p.team === player!.team && p.isGivingClues === true) {
           return true;
@@ -121,8 +145,7 @@ export class Impl implements Methods<InternalState> {
 
       if (!playerGivingClues) {
         player!.isGivingClues = true;
-      }else
-      {
+      } else {
         return Response.error("There is already one clue giver in your team");
       }
     }
@@ -131,63 +154,74 @@ export class Impl implements Methods<InternalState> {
   }
 
   bid(state: InternalState, userId: UserId, ctx: Context, request: IBidRequest): Response {
-    if(state.gameStatus != GameStatus.AUCTION){
+
+    let player = state.players.find((p) => p.id === userId);
+    if (!player) {
+      return Response.error("Player is undefined");
+    }
+
+    if (state.gameStatus != GameStatus.AUCTION) {
       return Response.error("It's not a bidding time!");
     }
 
-    let player = state.players.find((p) => p.id === userId);
-
-    if(player?.isGivingClues === false){
+    if (player?.isGivingClues === false) {
       return Response.error("You are not bidding");
+    }
+
+    if (request.hints != 0 && request.hints < state.roundInfo!.cards) {
+      return Response.error("You can't bid less than cards on board");
     }
 
     let teamA = state.teams.find((p) => p.color === player!.team);
     let teamB = state.teams.find((p) => p.color != player!.team);
 
-    if(request.hints === 0)
-    {
-      state.currentTurn = teamB!.color;
+    if (request.hints === 0) {
+      if (teamA!.bid! > teamB!.bid!) {
+        state.currentTurn = teamB!.color;
 
-      state.gameStatus = GameStatus.GUESSING;
-      return Response.ok();
+        state.gameStatus = GameStatus.GUESSING;
+        return Response.ok();
+      }
+      else { return Response.error("You cant accept opponent bid if your is lower"); }
     }
 
-    if(teamA!.bid! < teamB!.bid!){
+    if (teamA!.bid! < teamB!.bid!) {
       return Response.error("Your offer is already lower");
-    }else if(request.hints >= teamB!.bid!)
-    {
+    } else if (request.hints >= teamB!.bid!) {
       return Response.error("Your offer must be lower");
     }
-    else{
+    else {
       teamA!.bid = request.hints;
     }
     return Response.ok();
   }
 
   giveClue(state: InternalState, userId: UserId, ctx: Context, request: IGiveClueRequest): Response {
-    if(state.gameStatus != GameStatus.AUCTION){
+
+    let player = state.players.find((p) => p.id === userId);
+    if (!player) {
+      return Response.error("Player is undefined");
+    }
+
+    if (state.gameStatus != GameStatus.GUESSING) {
       return Response.error("It's not a guessing time!");
     }
 
-    let player = state.players.find((p) => p.id === userId);
-
-    if(player?.team != state.currentTurn){
+    if (player?.team != state.currentTurn) {
       return Response.error("You not your turn");
     }
 
-    if(player?.isGivingClues === false){
+    if (player?.isGivingClues === false) {
       return Response.error("You are not giving clues");
     }
 
     let teamA = state.teams.find((p) => p.color === player!.team);
 
-    if(teamA!.bid!>0)
-    {
+    if (teamA!.bid! > 0) {
       teamA!.bid!--;
       state.cards[request.word].hints.push(request.hint);
     }
-    else
-    {
+    else {
       return Response.error("You don't have any hints left");
     }
 
@@ -195,33 +229,48 @@ export class Impl implements Methods<InternalState> {
   }
 
   guessWord(state: InternalState, userId: UserId, ctx: Context, request: IGuessWordRequest): Response {
-    if(state.gameStatus != GameStatus.AUCTION){
+
+    let player = state.players.find((p) => p.id === userId);
+    if (!player) {
+      return Response.error("Player is undefined");
+    }
+
+    if (state.gameStatus != GameStatus.GUESSING) {
       return Response.error("It's not a guessing time!");
     }
 
-    let player = state.players.find((p) => p.id === userId);
-
-    if(player?.team != state.currentTurn){
+    if (player?.team != state.currentTurn) {
       return Response.error("You not your turn");
     }
 
-    if(player?.isGivingClues === false){
+    if (player?.isGivingClues === true) {
       return Response.error("You are not guessing");
     }
-    if(state.cards[request.word].word === request.guess)
-    {
+    var length = state.cards[request.word].word.length / 3 > 3 ? 3 : state.cards[request.word].word.length / 3
+    if (state.cards[request.word].word === request.guess || levenstein(state.cards[request.word].word, request.guess) < length) {
       state.cards[request.word].guessed = true;
+
+      state.roundInfo!.timeLeft += 10;
+
+      let notGuessed = state.roundInfo!.board!.find((p) => p.guessed === false);
+      if (!notGuessed) {
+        let team = state.teams.find((p) => p.color === state.currentTurn);
+        team!.points++;
+
+        state.gameStatus = GameStatus.ROUND_ENDED;
+
+        return Response.ok();
+      }
     }
-    else
-    {
+    else {
       state.cards[request.word].guesses.push(request.guess);
+      state.roundInfo!.timeLeft -= 3;
     }
-    
     return Response.ok();
   }
 
   timeEnd(state: InternalState, userId: UserId, ctx: Context, request: ITimeEndRequest): Response {
-        
+
     let team = state.teams.find((p) => p.color != state.currentTurn);
     team!.points++;
 
@@ -232,6 +281,18 @@ export class Impl implements Methods<InternalState> {
 
   getUserState(state: InternalState, userId: UserId): PlayerState {
     return state;
+  }
+
+  onTick(state: InternalState, ctx: Context, timeDelta: number): void {
+    if (state.gameStatus === GameStatus.GUESSING) {
+      state.roundInfo!.timeLeft--;
+      if (state.roundInfo!.timeLeft <= 0) {
+        let team = state.teams.find((p) => p.color != state.currentTurn);
+        team!.points++;
+
+        state.gameStatus = GameStatus.ROUND_ENDED;
+      }
+    }
   }
 }
 
@@ -245,4 +306,25 @@ function createPlayer(id: UserId): PlayerInfo {
 
 function chooseCards(words: string[], num: number): Card[] {
   return [...Array(num).keys()].map((_) => ({ word: words.pop()!, guessed: false, hints: [], guesses: [] }));
+}
+
+function levenstein(a: string, b: string) {
+  var b = b + "", m = [], i, j, min = Math.min;
+
+  if (!(a && b)) return (b || a).length;
+
+  for (i = 0; i <= b.length; m[i] = [i++]);
+  for (j = 0; j <= a.length; m[0][j] = j++);
+
+  for (i = 1; i <= b.length; i++) {
+    for (j = 1; j <= a.length; j++) {
+      m[i][j] = b.charAt(i - 1) == a.charAt(j - 1)
+        ? m[i - 1][j - 1]
+        : m[i][j] = min(
+          m[i - 1][j - 1] + 1,
+          min(m[i][j - 1] + 1, m[i - 1][j] + 1))
+    }
+  }
+
+  return m[b.length][a.length];
 }
