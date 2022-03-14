1b409fbf1ce1568f42010d0e4b9f1ef31b498461
Sebastian Rachwał <sebuh@tlen.pl>
2022-03-14 15:59:53 +0100

    - add eslint - add prettier

--- client/web/src/App.tsx
+++ client/web/src/App.tsx
@@ -1,21 +1,19 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import Title from "./Title";
-import Game from "./Game";
+import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Title from './Title';
 
 function App() {
-  return (
-    <div>
-      <Router>
+    return (
         <div>
-          <Switch>
-            <Route exact path="/" component={() => <Title />} />
-            <Route path="/game" component={() => <Game />} />
-          </Switch>
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={() => <Title />} />
+                    </Switch>
+                </div>
+            </Router>
         </div>
-      </Router>
-    </div>
-  );
+    );
 }
 
 export default App;
