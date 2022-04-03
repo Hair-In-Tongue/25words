9334fb7bc41627525fdfe2caa1617494b38db102
Szymon <szymon.sledziewski98@gmail.com>
2022-04-03 17:34:23 +0200

    Theme types for styled-components

---
+++ client/web/src/styled-components.d.ts
@@ -0,0 +1,9 @@
+// ./styled-components.d.ts
+import theme from './theme'
+
+type CustomTheme = typeof theme
+
+declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends CustomTheme {}
+}
